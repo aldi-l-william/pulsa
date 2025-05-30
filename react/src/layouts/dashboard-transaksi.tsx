8e04d66ae3cc85384a3dcf6f32b08e0035bdec7f
william <sibueawilliam99@gmail.com>
2025-05-30 09:45:10 +0700

    update transaksi

--- react/src/layouts/dashboard-transaksi.tsx
+++ react/src/layouts/dashboard-transaksi.tsx
@@ -6,6 +6,8 @@ const DashboardTransaksi = () => {
     const [error, setError] = useState<string | null>(null);
     const [showDialog, setShowDialog] = useState(false);
     const [itemList, setItemList] = useState<any>(null);
+    const [successlist, setSuccessList] = useState<any>(null);
+    const [isSuccessList, setIsSuccessList] = useState<boolean>(false);
 
     // Tambahkan state untuk nomor customer inputan
     const [customerNumberInput, setCustomerNumberInput] = useState("");
@@ -16,7 +18,9 @@ const DashboardTransaksi = () => {
         try {
             const res = await api.post('/buy-prepaid', { buyer_sku_code: itemList.buyer_sku_code,
                     customer_number: customerNumberInput });
-            console.log(res, "res data");
+            console.log(res.data.result, "res data");
+            setIsSuccessList(true);
+            setSuccessList(res.data.result);
             // Bisa reset nomor customer input setelah berhasil beli
             setCustomerNumberInput("");
             setShowDialog(false); // tutup dialog jika mau
@@ -50,6 +54,33 @@ const DashboardTransaksi = () => {
 
                 {!loading && !error && pricelist.length === 0 && (
                     <p>📭 Tidak ada data yang tersedia.</p>
+                )}
+
+                {isSuccessList && (
+                    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black/30 z-50">
+                        <div className="border border-black p-4 bg-white rounded shadow">
+                            <div className="flex justify-between items-center">
+                                <div>Sisa Saldo : <span className="font-bold">{successlist.data.buyer_last_saldo}</span></div>
+                                <div
+                                    onClick={() => {
+                                        setIsSuccessList(false);
+                                        setSuccessList(""); // reset successlist saat tutup
+                                    }}
+                                    className="bg-gray-400 rounded-full px-3 py-1 cursor-pointer text-white">x</div>
+                            </div>
+                            <div>Sku Code : <span className="font-bold">{successlist.data.buyer_sku_code}</span></div>
+                            <div>Customer No : <span className="font-bold">{successlist.data.customer_no}</span></div>
+                            <div>Message : <span className="font-bold">{successlist.data.message}</span></div>
+                            <div>Price : <span className="font-bold">{successlist.data.price}</span></div>
+                            <div>Ref_ID : <span className="font-bold">{successlist.data.ref_id}</span></div>
+                            <div>SN : <span className="font-bold">{successlist.data.sn}</span></div>
+                            <div>RC : <span className="font-bold">{successlist.data.rc}</span></div>
+                            <div>Status : <span className="font-bold">{successlist.data.status}</span></div>
+                            <div>Tele : <span className="font-bold">{successlist.data.tele}</span></div>
+                            <div>Wa : <span className="font-bold">{successlist.data.wa}</span></div>
+                        </div>
+                        
+                    </div>    
                 )}
 
                 {showDialog && (
