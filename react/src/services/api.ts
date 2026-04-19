011a66a0b18ca450592a6d8d2f32a3a883378016
Aldi Leonardo William <aldilwilliam2023@gmail.com>
2026-04-19 13:14:19 +0700

    ganti web url

--- react/src/services/api.ts
+++ react/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.mypulsa.my.id', // ganti sesuai domain API kamu
+  baseURL: 'https://rumahprofit.my.id', // ganti sesuai domain API kamu
   withCredentials: false,
 });
 
